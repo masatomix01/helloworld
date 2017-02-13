b640136edee7c2843ce938275a148f3c13535bcd
TakeoKugimiya <pbkugimiya@pb.primebrains.co.jp>
2017-02-13 16:21:00 +0900

    closes #5:wq:
    commit c7e428b148558f93afaafb5465c3f67a029fde82
    Author: TakeoKugimiya <pbkugimiya@pb.primebrains.co.jp>
    Date:   Mon Feb 13 16:16:51 2017 +0900
    
        close
        Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/nu/mine/kino/web/AddCalc.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * Copyright (c) 2014 Masatomi KINO and others. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *      Masatomi KINO - initial API and implementation
+ * $Id$
+ ******************************************************************************/
+//çÏê¨ì˙: 2017/02/10
+
+package nu.mine.kino.web;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Masatomi KINO
+ * @version $Revision$
+ */
+@RestController
+@RequestMapping("/add")
+public class AddCalc {
+
+    @RequestMapping(value = "/{param1}/{param2}", method = RequestMethod.GET)
+    public Object add(@PathVariable int param1, @PathVariable int param2) {
+        return param1 + param2;
+    }
+}
